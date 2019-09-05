239dbd7218572bf8cc9df072958aba963b4cbd1e
Naga Rohitha Nannapaneni <nagarohitha@usf.edu>
2019-09-05 16:25:14 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Day2_1_Project
+    /*if number of leave is greater than 7 then no
+     * if less then yes
+     * */
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         try
+            {
+                Console.WriteLine("How many days of leave do you want?");
+                int leave = int.Parse(Console.ReadLine());
+
+                if(leave > 7)
+                {
+                    Console.WriteLine("You can not request for leave");
+                }
+                else
+                {
+                    Console.WriteLine("You may request for leave");
+                }
+            }
+        catch
+            {
+                Console.WriteLine("you have an error");
+            }
+        }
+    }
+}
